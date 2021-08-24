0001037af3fdf537ff67a44568408521b4bc2e9d
Marina Miranda <mirandaxmarina@gmail.com>
2021-08-24 16:19:58 +0200

    new form

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :messages
   has_one_attached :image
+
+  validates :name, presence: true, length: { maximum: 18 }
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
