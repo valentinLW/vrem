4139b2097a64654dcf380dd4ced188e29584a6ec
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-08-24 14:42:08 +0200

    model validation and migration

--- db/migrate/20210824122841_create_invitations.rb
+++ db/migrate/20210824122841_create_invitations.rb
@@ -3,7 +3,7 @@ class CreateInvitations < ActiveRecord::Migration[6.0]
     create_table :invitations do |t|
       t.references :user, null: false, foreign_key: true
       t.references :event, null: false, foreign_key: true
-      t.integer :status
+      t.integer :status, default: 0
 
       t.timestamps
     end
