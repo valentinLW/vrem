bd05290c33de9e5a062ff6db35dd9edbd96ec565
Marina Miranda <mirandaxmarina@gmail.com>
2021-08-25 14:06:05 +0200

    messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,11 +6,18 @@ class MessagesController < ApplicationController
     @message.user = current_user
     if @message.save
       EventChannel.broadcast_to(@event, render_to_string(partial: "message", locals: { message: @message }))
-      redirect_to event_path(@event, anchor: "message-#{@message.id}")
+      redirect_to event_chat_path(@event, anchor: "message-#{@message.id}")
     else
       render "events/show"
     end
   end
+
+
+  def show
+    @event = Event.find(params[:event_id])
+    @message = Message.new
+  end
+
 
   private
 
