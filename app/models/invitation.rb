9f77478f06a837df4738272c3f295ed5525bbda5
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-08-25 12:34:40 +0200

    validate unique ivitations

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,4 +2,5 @@ class Invitation < ApplicationRecord
   belongs_to :user
   belongs_to :event
   enum status: %i[pending accepted rejected]
+  validates :user, uniqueness: { scope: :event, message: "user already invited" }
 end
