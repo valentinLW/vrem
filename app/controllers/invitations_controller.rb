1417216c29139acb573e98d6ee31765afcb8d4e2
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-08-25 15:25:39 +0200

    ta solution

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,9 +4,9 @@ class InvitationsController < ApplicationController
 
   def index
     @invitations = Invitation.where(event: @event)
-    @users = User.joins("LEFT JOIN invitations ON invitations.user_id = users.id").where("invitations.id IS NULL")
-    @allusers = User.all
     @invitation = Invitation.new
+    @not_guests = User.where.not(id: @event.invitations.pluck(:user_id))
+    raise
   end
 
   def create
