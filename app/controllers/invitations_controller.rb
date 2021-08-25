969c51fa367a89709a3b8b5d396b261c0e01762c
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-08-25 15:39:34 +0200

    host cant invite themselves

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -6,7 +6,6 @@ class InvitationsController < ApplicationController
     @invitations = Invitation.where(event: @event)
     @invitation = Invitation.new
     @not_guests = User.where.not(id: @event.invitations.pluck(:user_id))
-    raise
   end
 
   def create
