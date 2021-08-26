beacb6c0918cd317a727589056657e9aad728a73
Ruy Schneider <ruy.schneider@icloud.com>
2021-08-26 17:17:16 +0200

    minor fixxxes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
-      request.env['omniauth.origin'] || stored_location_for(resource) || events_path
-    end
+    request.env['omniauth.origin'] || stored_location_for(resource) || events_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
 end
