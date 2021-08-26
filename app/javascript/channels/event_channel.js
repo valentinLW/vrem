4fc7f00f2e1e973c52dc5b36755031bb7fb53771
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-08-26 09:40:17 +0200

    remove doubled author in broadcast

--- app/javascript/channels/event_channel.js
+++ app/javascript/channels/event_channel.js
@@ -7,7 +7,16 @@ const initEventCable = () => {
 
     consumer.subscriptions.create({ channel: "EventChannel", id: id }, {
       received(data) {
-        console.log(data)
+        const { groups: { user_id } } = /data-user-id="(?<user_id>\d)"/.exec(data)
+
+        const last_message = document.getElementById("messages").lastElementChild
+        const last_user_id = last_message.dataset.userId
+        const remove = last_user_id === user_id
+        if (remove) {
+          const i = last_message.querySelector("i")
+          i.parentElement.removeChild(i)
+        }
+
         messagesContainer.insertAdjacentHTML('beforeend', data);
       },
     });
