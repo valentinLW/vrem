c62b05cd377ef667c506fd30addf0b09f660c791
Ruy Schneider <ruy.schneider@icloud.com>
2021-08-26 15:44:34 +0200

    mapz fully working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,8 +28,10 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initEventCable } from '../channels/event_channel';
+import { initMapbox } from './init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   initEventCable();
+  initMapbox();
 });
