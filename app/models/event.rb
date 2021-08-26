e294fce4a51e12c8f95a8b02cc992f7b809278a1
Ruy Schneider <ruy.schneider@icloud.com>
2021-08-26 14:05:38 +0200

    should be fine

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,10 +2,12 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :messages
   has_many :invitations
+  has_many :invited_users, through: :invitations, source: :user
   has_one_attached :image
 
   validates :name, presence: true, length: { minimum: 5, maximum: 30 }
   validates :start_time, presence: true
   validates :end_time, presence: true
-  has_many :invited_users, through: :invitations, source: :user
+
+  geocoded_by :location
 end
