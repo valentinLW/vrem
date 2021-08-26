af3fa2dc24c339008f44b00140faadd1e5539e30
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-08-26 13:57:24 +0200

    add flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,8 +28,11 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initEventCable } from '../channels/event_channel';
+import { datepickr } from './init_flatpickr';
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   initEventCable();
+  datepickr();
 });
---
+++ app/javascript/packs/init_flatpickr.js
@@ -0,0 +1,13 @@
+import flatpickr from "flatpickr";
+
+function datepickr() {
+
+flatpickr(".datepicker", {
+  altInput: true
+});
+console.log("hello")
+}
+
+export {
+  datepickr
+}
