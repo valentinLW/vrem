4277f0f96e40cd23254bf79d63d07b2e71d2e903
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-08-26 10:04:15 +0200

    change classes for broadcast users

--- app/javascript/channels/event_channel.js
+++ app/javascript/channels/event_channel.js
@@ -8,6 +8,7 @@ const initEventCable = () => {
     consumer.subscriptions.create({ channel: "EventChannel", id: id }, {
       received(data) {
         const { groups: { user_id } } = /data-user-id="(?<user_id>\d)"/.exec(data)
+        const current_user_id = document.getElementById("messages").dataset.currentUserId
 
         const last_message = document.getElementById("messages").lastElementChild
         const last_user_id = last_message.dataset.userId
@@ -15,6 +16,10 @@ const initEventCable = () => {
         if (remove) {
           const i = last_message.querySelector("i")
           i.parentElement.removeChild(i)
+        }
+
+        if(current_user_id !== user_id) {
+          data = data.replaceAll("my-message", "others-message")
         }
 
         messagesContainer.insertAdjacentHTML('beforeend', data);
