2bc32836414c7b9c50d501888c14db331feb2c35
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-08-30 09:27:59 +0200

    before_action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
+  before_action :set_event
+
   def create
-    @event = Event.find(params[:event_id])
     @message = Message.new(message_params)
     @message.event = @event
     @message.user = current_user
@@ -13,15 +14,16 @@ class MessagesController < ApplicationController
     end
   end
 
-
   def show
-    @event = Event.find(params[:event_id])
     @message = Message.new
     @disable_footer = @disable_header = true
   end
 
-
   private
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
 
   def message_params
     params.require(:message).permit(:content)
